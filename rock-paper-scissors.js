22b448f5b64a4a058eda525b56b20d95e7d1333b
pram <91541258+pran-12@users.noreply.github.com>
2024-05-04 19:31:48 +0530

    Initial commit

---
+++ rock-paper-scissors.js
@@ -0,0 +1,74 @@
+let score =JSON.parse(localStorage.getItem('score')) || {
+  wins:0,
+  losses:0,
+  ties:0
+};
+
+  updateScoreElement();
+//to get the score out of localStorage we need to use method called getItem.now instead of setting the score to 0 we will use the values from localStorage for that we have to convert string into object now so we are using parse method 
+//console.log(JSON.parse(localStorage.getItem('score')));
+
+function playGame(playerMove){
+const computerMove = pickComputerMove();
+
+let result = '';
+if(playerMove === 'scissors'){
+if(computerMove === 'rock'){
+  result='You lose';
+}else if(computerMove === 'paper'){
+  result = 'You win';
+}else if(computerMove === 'scissors'){
+  result = 'Tie';
+}
+}else if(playerMove === 'paper'){
+if(computerMove === 'rock'){
+  result='You win';
+}else if(computerMove === 'paper'){
+  result = 'Tie';
+}else if(computerMove === 'scissors'){
+  result = 'You lose';
+}
+}else if(playerMove === 'rock'){
+if(computerMove === 'rock'){
+result='Tie';
+}else if(computerMove === 'paper'){
+  result = 'You lose';
+}else if(computerMove === 'scissors'){
+  result = 'You win';
+}
+}
+
+if (result === 'You win'){
+score.wins = score.wins + 1;
+} else if (result === 'You lose'){
+score.losses += 1;
+} else if(result === 'Tie'){
+score.ties += 1;
+}
+//localStorage only support strings,hear score is an object so with the help of JSON we are converting JSobject into String
+localStorage.setItem('score', JSON.stringify(score));
+
+updateScoreElement();
+document.querySelector('.js-result')
+.innerHTML = result;
+document.querySelector('.js-moves')
+.innerHTML = `You <img src="${playerMove}-emoji.png" alt="" class="move-icon">
+<img src="${computerMove}-emoji.png" alt="" class="move-icon"> Computer`;  
+}
+
+function updateScoreElement(){
+document.querySelector('.js-score')
+  .innerHTML = `Wins: ${score.wins},Losses: ${score.losses},Ties: ${score.ties}`;
+}    
+function pickComputerMove() {
+let computerMove = '';
+const randomNumber = Math.random();
+if(randomNumber >= 0 && randomNumber < 1/3){
+  computerMove = 'rock';
+}else if(randomNumber >= 1/3 && randomNumber < 2/3){
+  computerMove ='paper';
+}else{
+  computerMove ='scissors';
+}
+return computerMove;
+} 
